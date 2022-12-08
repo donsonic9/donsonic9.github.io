4e46fda15257e834f9ed2f747aa03fcbc05e86e3
donsonic9 <117784209+donsonic9@users.noreply.github.com>
2022-12-08 20:09:42 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+
+// JS perteneciente al index.html
+
+let cssChange = document.getElementById("csslink");
+// let cssChangelo = document.getElementById("csslinklo");
+let colorButton = document.getElementById("hero");
+// let logButton = document.getElementById("herolo");
+
+let counter = 0;
+
+colorButton.addEventListener("click", ()=>{
+
+  if (counter == 0) {
+    cssChange.setAttribute("href", "assets/css/custom-2.css");
+    counter = 1;
+  } else if (counter == 1) {
+    cssChange.setAttribute("href", "assets/css/custom-3.css");
+    counter = 2;
+  } else if (counter == 2) {
+    cssChange.setAttribute("href", "assets/css/custom-4.css");
+    counter = 3;
+  } else if (counter == 3) {
+    cssChange.setAttribute("href", "assets/css/custom-1.css");
+    counter = 0;
+  }
+
+  // console.log(counter);
+
+});
+
+// logButton.addEventListener("click", ()=>{
+
+//   if (counter == 0) {
+//     cssChangelo.setAttribute("href", "assets/css/login2.css");
+//     counter = 1;
+//   } else if (counter == 1) {
+//     cssChangelo.setAttribute("href", "assets/css/login3.css");
+//     counter = 2;
+//   } else if (counter == 2) {
+//     cssChangelo.setAttribute("href", "assets/css/login4.css");
+//     counter = 3;
+//   } else if (counter == 3) {
+//     cssChangelo.setAttribute("href", "assets/css/login.css");
+//     counter = 0;
+//   }
+
+//   // console.log(counter);
+
+// });
